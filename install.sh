f438a0c07480cb82c8d8a7682c096da96f6359ea
Long Yun <longdeqidao@gmail.com>
2017-05-04 17:29:03 +0800

    delete ackrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -s ~/mydotfile/zshrc ~/.zshrc
-ln -s ~/mydotfile/ackrc ~/.ackrc
+ln -s ~/mydotfile/gitconfig ~/.gitconfig
 ln -s ~/.config/nvim ~/.vim
 ln -s ~/.config/nvim/init.vim ~/.vimrc
 ln -s ~/.config/nvim/vimrc.bundles ~/.vimrc.bundles
