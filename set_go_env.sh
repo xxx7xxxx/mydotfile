524afa2622f3cc051b8fc0437e7e7bb751ad0868
Yun Long <longdeqidao@gmail.com>
2018-12-27 14:49:03 +0800

    update set_go_env

--- set_go_env.sh
+++ set_go_env.sh
@@ -1,7 +1,11 @@
-BASEDIR=`dirname $0`
-if [[ ! -z "${BASEDIR}" ]] && [[ "${BASEDIR}" != "$GOPATH" ]]; then
-    export GOPATH=${BASEDIR}
-    [[ ":$PATH:" != *":${GOPATH}/bin:"* ]] && export PATH="${GOPATH}/bin:$PATH"
-    alias kk="cd ${BASEDIR}"
-    alias jj="cd ${BASEDIR}/repo"
-fi
+export GOPATH=`dirname $0`
+[[ ":$PATH:" != *":${GOPATH}/bin:"* ]] && export PATH="${GOPATH}/bin:$PATH"
+alias kk="cd ${GOPATH}"
+local REPO
+case ${GOPATH} in
+    *easegateway)   REPO=${GOPATH}/src/github.com/megaease/easegateway ;;
+    *easestack)     REPO=${GOPATH}/src/github.com/hexdecteam/easestack ;;
+    *kubernetes)    REPO=${GOPATH}/src/github.com/kubernetes/kubernetes ;;
+    *etcd)          REPO=${GOPATH}/src/go.etcd.io/etcd ;;
+esac
+alias jj="cd ${REPO}"
