e5bbce42df39c40cdf2460d512ad67bcb1465f10
Yun Long <longdeqidao@gmail.com>
2021-11-26 11:00:57 +0800

    Add homebrew script

---
+++ homebrew.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo '
+autoenv
+autojump
+bash
+bat
+diff-so-fancy
+exa
+ffmpeg
+gnu-tar
+go
+goreleaser
+htop
+neovim
+python
+ripgrep
+wget
+youtube-dl
+zsh
+' | xargs brew install
